46bfc571b6e56ec4a7d7978c11c59efc05373591
Egan <ebisma@ucsc.edu>
2017-12-22 08:34:49 +0530

    added directions in home directory

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const router = require('./routes/image');
 
 const axios = require("axios");
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hey use</h1> <h2><ul><li>/api/imagesearch/?q=WhatYouWannaSearch&offset=numberOfResultsYouWannaSee to search for an image.</li> <li>/api/latest/imagesearch for image search history</li></h2>")
+})
+
 app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
